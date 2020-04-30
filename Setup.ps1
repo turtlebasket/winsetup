306f3e7348be6c78db3824b20929eb094d01aea8
turtlebasket <turtlebasket@gmail.com>
2020-04-29 22:19:25 -0700

    more scripty stuff

--- Setup.ps1
+++ Setup.ps1
@@ -1,8 +1,9 @@
 ﻿# Install Chocolatey
 
-Write-Output "Install PowerShell."
+Write-Output "Install Chocolatey."
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-choco.exe install -y vim
+Write-Output "Install preferred applications."
+choco.exe install -y vim sharex
 
 # WSL2 coming soon to non-insiders windows releases. Figure out installation once it's here.
