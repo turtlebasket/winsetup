0837d397fff104a86be352a438d8307e6d39395e
turtlebasket <turtlebasket@gmail.com>
2021-01-05 23:54:40 -0800

    update pwsh config

--- Configs/Powershell/Microsoft.PowerShell_profile.ps1
+++ Configs/Powershell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,20 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-Set-Theme Agnoster
-Remove-Alias ls
+Import-Module -Name Terminal-Icons
+Import-Module posh-alias
+# Import-Module oh-my-posh
+# Set-PoshPrompt -Theme ~/.turtlebasket_omp.json
+# $ThemeSettings.PromptSymbols.SegmentForwardSymbol = [char]::ConvertFromUtf32("0xE0BC")
+
 Remove-Alias rm
-Set-Alias cat bat
+Remove-Alias cp
+Remove-Alias mv
 Set-Alias vim gvim
-Set-Alias gb 'C:\Program Files\Git\bin\bash.exe'
+Set-Alias nb 'C:\Program Files\Git\bin\bash.exe'
 Set-Alias wb bash
+Add-Alias ls 'ls.exe --color'
 
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
 Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+Invoke-Expression (&starship init powershell)
